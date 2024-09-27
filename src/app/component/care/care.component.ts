edd159845ee41db73120cea5fb1bcbd32c926504
Kulum-eng <ale29rmrz@gmail.com>
2024-09-27 15:45:44 -0600

    m

---
+++ src/app/component/care/care.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-care',
+  imports: [],
+  standalone: true,
+  templateUrl: './care.component.html',
+  styleUrls: ['./care.component.css']
+})
+export class CareComponent {
+  
+}
--- src/app/dash-card/dash-card.component.ts
+++ src/app/dash-card/dash-card.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CareComponent } from '../component/care/care.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dash-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, CareComponent],
   templateUrl: './dash-card.component.html',
   styleUrl: './dash-card.component.css'
 })
