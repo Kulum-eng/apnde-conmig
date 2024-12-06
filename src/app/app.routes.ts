4db8924063dc00239b877ef9b6de734782aa5bce
Kulum-eng <120421270+Kulum-eng@users.noreply.github.com>
2024-12-06 09:29:48 -0600

    dsa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,25 +8,26 @@ import { AvatarComponent } from './component/avatar/avatar.component';
 import { NotificacionesComponent } from './component/notificaciones/notificaciones.component';
 import { MusicaComponent } from './component/musica/musica.component';
 import { AyudaComponent } from './component/ayuda/ayuda.component';
-import { OperacionesComponent } from './component/operaciones/operaciones.component'; 
+import { OperacionesComponent } from './component/operaciones/operaciones.component';
 import { RestaComponent } from './component/resta/resta.component';
 import { MultiplicacionComponent } from './component/multiplicacion/multiplicacion.component';
 import { DivisionComponent } from './component/division/division.component';
 import { SumaComponent } from './component/suma/suma.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'inicio', component: InicioComponent },
-    { path: 'bienvenida', component: BienvenidaComponent },
-    { path: 'ajustes', component: AjustesComponent },
-    { path: 'carrito', component: CarritoComponent },
-    { path: 'avatar', component: AvatarComponent },
-    { path: 'notificaciones', component: NotificacionesComponent },
-    { path: 'musica', component: MusicaComponent },
-    { path: 'ayuda', component: AyudaComponent },
-    { path: 'operaciones', component: OperacionesComponent },
-    { path: 'resta', component: RestaComponent },
-    { path: 'multiplicacion', component: MultiplicacionComponent },
-    { path: 'division', component: DivisionComponent },
-    { path: 'suma' , component: SumaComponent}
+  { path: '', component: HomeComponent },
+  { path: 'inicio', component: InicioComponent }, 
+  { path: 'bienvenida', component: BienvenidaComponent, canActivate: [AuthGuard] },
+  { path: 'ajustes', component: AjustesComponent, canActivate: [AuthGuard] },
+  { path: 'carrito', component: CarritoComponent, canActivate: [AuthGuard] },
+  { path: 'avatar', component: AvatarComponent, canActivate: [AuthGuard] },
+  { path: 'notificaciones', component: NotificacionesComponent, canActivate: [AuthGuard] },
+  { path: 'musica', component: MusicaComponent, canActivate: [AuthGuard] },
+  { path: 'ayuda', component: AyudaComponent, canActivate: [AuthGuard] },
+  { path: 'operaciones', component: OperacionesComponent, canActivate: [AuthGuard] },
+  { path: 'resta', component: RestaComponent, canActivate: [AuthGuard] },
+  { path: 'multiplicacion', component: MultiplicacionComponent, canActivate: [AuthGuard] },
+  { path: 'division', component: DivisionComponent, canActivate: [AuthGuard] },
+  { path: 'suma', component: SumaComponent, canActivate: [AuthGuard] }
 ];
